a770e8e6e2bc7b873fa3565306e01155f499abb4
Octavio Roncal <octavioroncal11@gmail.com>
2024-02-14 16:39:40 +0000

    ignored tests

--- legio2packagejson/test/MainTest.java
+++ legio2packagejson/test/MainTest.java
@@ -1,9 +1,11 @@
 import io.intino.legio2packagejson.Legio2PackageJson;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class MainTest {
 
 	@Test
+	@Ignore
 	public void name() {
 		Legio2PackageJson.main(new String[]{"/Users/oroncal/workspace/sandbox/sinergia/sinergiadeseo/automoviles/core-elements", null, "/Users/oroncal/workspace/infrastructure/legio2maven/temp/legio-deployer-credentials.tsv"});
 	}
