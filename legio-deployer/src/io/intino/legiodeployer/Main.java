482876735dce31a5b46eb37b64bbcb7188fc8b40
Octavio Roncal <octavioroncal11@gmail.com>
2024-02-15 09:54:00 +0000

    fixed arguments

--- legio-deployer/src/io/intino/legiodeployer/Main.java
+++ legio-deployer/src/io/intino/legiodeployer/Main.java
@@ -13,11 +13,15 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		if (args.length < 5) return;
+		if (args.length != 5) {
+			System.err.println("Expected args in order: {module_directory} {project_file} {cesar_url} {cesar_token}");
+			return;
+		}
+
 		Configuration configuration = configuration(args[0], args[1]);
 		System.out.println("Loaded configuration.");
 		ApiAccessor accessor = createAccessor(args[2], args[3]);
-		ArtifactDeployer deployer = new ArtifactDeployer(configuration, accessor, new File(args[5]));
+		ArtifactDeployer deployer = new ArtifactDeployer(configuration, accessor, new File(args[4]));
 		for (Configuration.Deployment deployment : configuration.artifact().deployments()) {
 			try {
 				deployer.deployTo(deployment);
@@ -31,7 +35,7 @@ public class Main {
 
 	private static Configuration configuration(String dir, String projectFile) {
 		File file = new File(new File(dir), "artifact.legio");
-		return new ConfigurationLoader().load(file, new File(projectFile));
+		return new ConfigurationLoader().load(file, new File(projectFile).exists() ? new File(projectFile) : null);
 	}
 
 	private static ApiAccessor createAccessor(String url, String token) {
--- legio2packagejson/src/io/intino/legio2packagejson/Legio2PackageJson.java
+++ legio2packagejson/src/io/intino/legio2packagejson/Legio2PackageJson.java
@@ -7,9 +7,13 @@ import java.io.File;
 
 public class Legio2PackageJson {
 	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.err.println("Expected args in order: {module_directory} {project_file} {credentials_file}");
+			return;
+		}
 		File root = new File(args[0]);
 		File file = new File(root, "artifact.legio");
-		Configuration conf = new ConfigurationLoader().load(file, args.length > 1 && args[1] != null ? new File(args[1]) : null);
-		new PackageJsonCreator(conf, new File(args[1]), new File(root, "node_modules")).createPackageFile(root);
+		Configuration conf = new ConfigurationLoader().load(file, args[1] != null && new File(args[1]).exists() ? new File(args[1]) : null);
+		new PackageJsonCreator(conf, new File(args[2]), new File(root, "node_modules")).createPackageFile(root);
 	}
 }
