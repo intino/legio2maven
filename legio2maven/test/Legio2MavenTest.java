dbba03c4fb1a96835a41cbe4278e2aa19028faf3
Octavio Roncal <octavioroncal11@gmail.com>
2024-10-01 09:11:25 +0100

    fixed pom template

--- legio2maven/test/Legio2MavenTest.java
+++ legio2maven/test/Legio2MavenTest.java
@@ -5,6 +5,6 @@ public class Legio2MavenTest {
 
 	@Test
 	public void testProject() {
-		Legio2Maven.main(new String[]{"/Users/oroncal/workspace/infrastructure/archetype", null});
+		Legio2Maven.main(new String[]{"/Users/oroncal/Downloads/", "/Users/oroncal/Downloads/project.legio"});
 	}
 }
